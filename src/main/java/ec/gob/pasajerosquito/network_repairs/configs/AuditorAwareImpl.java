13ca22ce3fc9f9807e10aa113785b25f3e225993
Miguel Mendosa <138393857+Mike37Jet@users.noreply.github.com>
2025-04-03 19:53:24 -0500

    Refactor AuditorAwareImpl para usar anotación @NonNull y mejorar la legibilidad del código

--- src/main/java/ec/gob/pasajerosquito/network_repairs/configs/AuditorAwareImpl.java
+++ src/main/java/ec/gob/pasajerosquito/network_repairs/configs/AuditorAwareImpl.java
@@ -1,14 +1,14 @@
 package ec.gob.pasajerosquito.network_repairs.configs;
 
 import org.springframework.data.domain.AuditorAware;
-
+import org.springframework.lang.NonNull;
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
-    @SuppressWarnings("NullableProblems")
+    @NonNull
     public Optional<String> getCurrentAuditor() {
-        // TODO replace this with authenticated user
+        //TODO replace this with authenticated user
         return Optional.of("pasajerosquito");
     }
 }
